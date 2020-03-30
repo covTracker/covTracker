4c36acbeb3b58d445cb175cecbb2ada04bdcb693
Art <artelo.studio@gmail.com>
2020-03-30 22:36:16 +0000

    pushed to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'roster#index'
+  resources :roster
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
